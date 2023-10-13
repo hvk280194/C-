ec941f766efa2161c64006de08b5a04ec71bdf8f
unknown <harshvardhan280194@gmail.com>
2023-10-13 17:42:26 +0200

    asas

---
+++ Old TicTacToe/include/Game.h
@@ -0,0 +1,14 @@
+#ifndef _GAME_H_
+#define _GAME_H_
+
+#include "Board.h"
+
+class Game {
+public:
+	Game(Board* board);
+	~Game() = default;
+	void run();
+private:
+	Board* board_;
+};
+#endif // !_GAME_H_
---
+++ Old TicTacToe/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Board.h"
+#include "Game.h"
+
+int main() {
+	Board* board = new Board();
+	Game game(board);
+	game.run();
+
+	delete board;
+	return 0;
+}
